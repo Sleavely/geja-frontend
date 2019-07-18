70972254dabd02ac662b66f83316392b8f46f2b2
Joakim Hedlund <joakim.hedlund@danielwellington.com>
2019-07-18 10:51:47 +0200

    Clean up ProductPage

--- src/lib/ProductPage/index.js
+++ src/lib/ProductPage/index.js
@@ -1,4 +1,4 @@
-import React, { Component, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Markdown from 'react-markdown'
 import { useCart } from "use-cart"
 
@@ -12,97 +12,18 @@ import {
   Button,
   Card,
   Carousel,
-  Select,
   Skeleton,
   Statistic,
 } from 'antd'
-
-import RelatedProducts from '../RelatedProducts'
 
 const {
   Title,
   Paragraph,
 } = Typography
 
-export class OldProductPage extends Component {
-
-  static defaultProps = {
-    productId: undefined,
-    sizes: ['Liten', 'Stor'].map(size => <Select.Option key={size.toLowerCase()}>{size}</Select.Option>)
-  }
-
-  state = {
-    loading: false,
-    selectedSize: undefined,
-  }
-
-  addToCart = () => {
-    this.setState({ loading: true })
-    console.log(`Adding ${this.state.selectedSize} ${this.props.productId} to cart.`)
-    setTimeout(() => {
-      this.setState({ loading: false })
-    }, 2000)
-  }
-
-  handleSizeChange = (value) => {
-    console.log(`Selected: ${value}`)
-    this.setState({ selectedSize: value })
-  }
-
-  render() {
-    return (
-      <div className="product" style={{ padding: 24, background: '#fff', minHeight: 160 }}>
-        <Row gutter={16}>
-          <Col md={12}>
-            <Title>Halsband Classic 790</Title>
-            <Paragraph>
-              <Statistic title="" value={7199} suffix="SEK" groupSeparator=" " />
-            </Paragraph>
-            <div className="slider-mobile">
-              <Carousel autoplay>
-                <img style={{ maxWidth: '100%' }} src="/images/pexels-photo-247115.jpeg" alt="" />
-                <img style={{ maxWidth: '100%' }} src="/images/pixabay-3090593_1920.jpeg" alt="" />
-              </Carousel>
-            </div>
-            <Paragraph>
-              Classic 790 är ett ett av våra vackraste och mest exklusiva halsband. Dom tjocka pärlorna fångar blickar och din accessoar blir en av kvällens samtalsämnen.
-            </Paragraph>
-            <Paragraph>
-              Halsband Classic 790 kommer från en bortglömd pärlfiskarby i sydöstra Asien där man fortfarande känner av effekterna från Vietnamkriget. Halsbandet är resultatet av åratals slit och en hel samhällighets förenade krafter. Slipat till perfektion av ett underbetalt barn. Polerat av en svältande sexbarnsmamma. Köper du inte det här halsbandet kommer alla dö en hemsk död.
-            </Paragraph>
-
-            <Card>
-            <Select
-              placeholder="Välj storlek"
-              notFoundContent="Det saknas storleksinformation för den här produkten."
-              onChange={this.handleSizeChange}
-              size="large" style={{ display: 'block', marginBottom: 16 }}
-            >
-              {this.props.sizes}
-            </Select>
-                  <Button type="primary" block icon="shopping" size="large" loading={this.state.loading} onClick={this.addToCart}>
-                    Lägg i varukorgen
-                  </Button>
-
-            </Card>
-            <Alert style={{ marginTop: '18px' }} message="Produkten är tillfälligt slut i lager. Det kan därför ta upp till tre veckor innan den levereras." type="warning" showIcon />
-          </Col>
-          <Col md={12}>
-            <div className="slider-desktop">
-              <Carousel autoplay>
-                <img style={{ maxWidth: '100%' }} src="/images/pexels-photo-247115.jpeg" alt="" />
-                <img style={{ maxWidth: '100%' }} src="/images/pixabay-3090593_1920.jpeg" alt="" />
-              </Carousel>
-            </div>
-          </Col>
-        </Row>
-        <RelatedProducts productId={this.props.productId} style={{ marginTop: 2000 }} />
-      </div>
-    )
-  }
-}
-
-const API_BASE = 'https://aws.triplehead.net/geja'
+const {
+  REACT_APP_API_BASE_PATH: API_BASE_PATH
+} = process.env
 
 export default function ProductPage({ slug }) {
   const { addItem } = useCart()
@@ -110,9 +31,9 @@ export default function ProductPage({ slug }) {
   const [error, setError] = useState(null)
 
   useEffect(() => {
-    fetch(`${API_BASE}/contentful/products/${slug}`)
+    fetch(`${API_BASE_PATH}/contentful/products/${slug}`)
       .then((data) => {
-        //TODO: shouldnt the router be responsible to 404-like management?
+        //TODO: shouldnt the router be responsible for 404-like management?
         console.log('product response', data)
         if(!data.ok) {
           if(data.status === 404) {
@@ -154,8 +75,8 @@ export default function ProductPage({ slug }) {
               <div className="slider-mobile">
                 <Carousel autoplay>
                   {
-                    product.image.map((image) => (
-                      <img src={image.file.url} alt="" />
+                    product.image.map((image, i) => (
+                      <img key={i} src={image.file.url} alt="" />
                     ))
                   }
                 </Carousel>
@@ -176,8 +97,8 @@ export default function ProductPage({ slug }) {
               <div className="slider-desktop">
                 <Carousel autoplay>
                   {
-                    product.image.map((image) => (
-                      <img src={image.file.url} alt="" />
+                    product.image.map((image, i) => (
+                      <img key={i} src={image.file.url} alt="" />
                     ))
                   }
                 </Carousel>
