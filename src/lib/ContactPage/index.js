8f2a7e645707947c58cffc20a41ba041782f05cf
Joakim Hedlund <contact@joakimhedlund.com>
2019-08-17 18:46:26 +0200

    Complete UI for contact page; cue API call

--- src/lib/ContactPage/index.js
+++ src/lib/ContactPage/index.js
@@ -1,9 +1,11 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import {
+  Alert,
   Button,
   Form,
   Input,
+  Spin,
   Typography,
 } from 'antd'
 
@@ -24,16 +26,22 @@ const ContactForm = Form.create({
       }
     })
   }
+
+  const propsWithoutFormOrSubmitHandler = Object.entries(props).reduce((out, [key, value]) => {
+    if(!['form', 'onSubmit'].includes(key)) out[key] = value
+    return out
+  }, {})
+
   return (
-    <Form {...props} onSubmit={handleSubmit}>
-      <Form.Item label="Din e-postadress" extra="Ange den e-postadress du vill att vi svarar till.">
+    <Form {...propsWithoutFormOrSubmitHandler} onSubmit={handleSubmit}>
+      <Form.Item hasFeedback label="Din e-postadress" extra="Ange den e-postadress du vill att vi svarar till.">
         {getFieldDecorator('email', {
-          rules: [{ required: true, type: 'email', message: 'Din e-postadress är obligatorisk' }],
-        })(<Input />)}
+          rules: [{ required: true, transform: (v) => v && v.trim(), type: 'email', message: 'Din e-postadress är obligatorisk' }],
+        })(<Input type="email" />)}
       </Form.Item>
-      <Form.Item label="Meddelande" extra="Om du är befintlig kund, glöm inte att bifoga din betalningsreferens från orderbekräftelsen." htmlFor="textarea">
+      <Form.Item hasFeedback label="Meddelande" extra="Om du är befintlig kund, glöm inte att bifoga din betalningsreferens från orderbekräftelsen." htmlFor="textarea">
         {getFieldDecorator('message', {
-          rules: [{ required: true, message: 'Du måste fylla i ett meddelande' }],
+          rules: [{ required: true, transform: (v) => v && v.trim(), message: 'Du måste fylla i ett meddelande' }],
         })(<Input.TextArea rows={4} />)}
       </Form.Item>
       <Form.Item>
@@ -46,12 +54,23 @@ const ContactForm = Form.create({
 })
 
 export default function ContactPage () {
+  const [isSubmitting, setIsSubmitting] = useState(false)
+  const [submitted, setSubmitted] = useState(false)
+
   useEffect(() => {
     document.title = `Kontakt | GEJA Smycken`
   })
 
+  useEffect(() => {
+    setIsSubmitting(false)
+  }, [submitted])
+
   const submitHandlerPostValidation = (values) => {
     console.log('sending your datas to N(A)SA', values)
+    setIsSubmitting(true)
+    setTimeout(() => {
+      setSubmitted(true)
+    }, 1500)
   }
 
   return (
@@ -59,7 +78,16 @@ export default function ContactPage () {
       <div style={{ maxWidth: 600 }}>
         <Title>Kontakta oss</Title>
         <Paragraph>Vi försöker att svara på alla meddelanden inom 48 timmar.</Paragraph>
-        <ContactForm onSubmit={submitHandlerPostValidation} hideRequiredMark={true} />
+        <Spin spinning={isSubmitting}>
+          {submitted
+          ? <Alert
+              message="Ditt meddelandet har skickats."
+              type="success"
+              showIcon
+            />
+          : <ContactForm onSubmit={submitHandlerPostValidation} hideRequiredMark={true} />
+          }
+        </Spin>
       </div>
     </div>
   )
